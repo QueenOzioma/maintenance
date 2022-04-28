8a9be4567bb87fb5495e5697e489b2c2147cc711
Queen <Queen@DESKTOP-E6HRNJV>
2022-04-28 09:54:06 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace maintenance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.product = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stock = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Avaliablilty = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.product,
+            this.stock,
+            this.amount,
+            this.Avaliablilty});
+            this.dataGridView1.Location = new System.Drawing.Point(55, 20);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(617, 327);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // savebtn
+            // 
+            this.savebtn.Location = new System.Drawing.Point(587, 387);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(134, 51);
+            this.savebtn.TabIndex = 1;
+            this.savebtn.Text = "Save";
+            this.savebtn.UseVisualStyleBackColor = true;
+            this.savebtn.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // product
+            // 
+            this.product.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.product.HeaderText = "Product name";
+            this.product.MinimumWidth = 6;
+            this.product.Name = "product";
+            this.product.Width = 125;
+            // 
+            // stock
+            // 
+            this.stock.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.stock.HeaderText = "Price";
+            this.stock.MinimumWidth = 6;
+            this.stock.Name = "stock";
+            this.stock.Width = 69;
+            // 
+            // amount
+            // 
+            this.amount.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.amount.HeaderText = "Stock";
+            this.amount.MinimumWidth = 6;
+            this.amount.Name = "amount";
+            this.amount.Width = 72;
+            // 
+            // Avaliablilty
+            // 
+            this.Avaliablilty.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Avaliablilty.HeaderText = "Availablity";
+            this.Avaliablilty.MinimumWidth = 6;
+            this.Avaliablilty.Name = "Avaliablilty";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.savebtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "product maintenance";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn product;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stock;
+        private System.Windows.Forms.DataGridViewTextBoxColumn amount;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Avaliablilty;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace maintenance
+{
+    public partial class Form1 : Form
+    {
+        //globals
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\" + "UserInterface";
+
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = folderPath + "\\" + "Inventory" + ".txt";
+            if (!Directory.Exists(folderPath)) Directory.CreateDirectory(folderPath);
+            if (!File.Exists(path)) File.CreateText(path);
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string line;
+                    while((line = reader.ReadLine()) != null)
+                    {
+                        if (line != "")
+                        {
+                            PopulateDataGridview(line);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void PopulateDataGridview(string line)
+        {
+            string[] temp = line.Split('/');
+            DataGridViewRow rowtoadd = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+            rowtoadd.Cells[0].Value = temp[0];
+            rowtoadd.Cells[1].Value = temp[1];
+            rowtoadd.Cells[2].Value = temp[2];
+            if(temp[3] == "1")
+            {
+                rowtoadd.Cells[3].Value = true;
+            }
+
+            else
+            {
+                rowtoadd.Cells[3].Value = false;
+            }
+            dataGridView1.Rows.Add(rowtoadd);
+
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            string path = folderPath + "\\" + "Inventory" + ".txt";
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    string line;
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        if(row.Cells[3].Value != null)
+                        {
+                            line = row.Cells[0].Value.ToString() + "/" + row.Cells[1].Value.ToString() + "/" + row.Cells[2].Value.ToString();
+                            if ((bool)row.Cells[3].Value == true)
+                            {
+                                line += "/" + "1";
+                            }
+
+                            else
+                            {
+                                line += "/" + "0";
+                            }
+                            writer.WriteLine(line);
+                        }
+                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
